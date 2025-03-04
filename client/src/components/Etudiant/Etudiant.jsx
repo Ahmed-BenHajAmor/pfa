e41185eaee98a377185168ac62f1dd57ef6d82b8
Ahmed <ahmed.benhajamorr@gmail.com>
2025-03-04 11:55:54 +0100

    charts

--- client/src/components/Etudiant/Etudiant.jsx
+++ client/src/components/Etudiant/Etudiant.jsx
@@ -16,10 +16,10 @@ function Etudiant() {
               <Button>Log out</Button>
             </div>
             <div className='etudiant-stats '>
-              <div style={{boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px'}}><MyGraph/></div>
-              <div style={{boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px'}}><CoherenceChart/></div>
-              <div style={{boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px'}}><JustificationImpactChart/></div>
-              <div style={{boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px'}} ><AbsenceTable/></div>
+              <div style={{boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><MyGraph/></div>
+              <div style={{boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><CoherenceChart/></div>
+              <div style={{boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><JustificationImpactChart/></div>
+              <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} ><AbsenceTable/></div>
               
               
             </div>
