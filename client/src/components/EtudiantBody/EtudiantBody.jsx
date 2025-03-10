47b8fc4e68d9ba607226b238a4c01dbe8f64f4b8
Wassim1920 <nnassi377@gmail.com>
2025-03-10 16:24:21 +0100

    Adjusting Etudient

--- client/src/components/EtudiantBody/EtudiantBody.jsx
+++ client/src/components/EtudiantBody/EtudiantBody.jsx
@@ -12,10 +12,10 @@ function EtudiantBody() {
           <Button>Log out</Button>
         </div>
         <div className='etudiant-stats ' style={{display: "flex" ,flexWrap:'wrap'}}>
-          <div style={{flexBasis:'200', flexGrow:'1' ,boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><MyGraph/></div>
-          <div style={{flexBasis:'200', flexGrow:'1' , boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><CoherenceChart/></div>
-          <div style={{flexBasis:'200', flexGrow:'1' , boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><JustificationImpactChart/></div>
-          <div style={{flexBasis:'200', flexGrow:'2' , display: 'flex', justifyContent: 'center', alignItems: 'center'}} ><AbsenceTable/></div>
+          <div style={{flexBasis:'200', flexGrow:'1' ,boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px'}}><MyGraph/></div>
+          <div style={{flexBasis:'200', flexGrow:'1' , boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px'}}><CoherenceChart/></div>
+          <div style={{flexBasis:'200', flexGrow:'1' , boxShadow:'0px 2px 6px 0px rgb(211, 211, 214)',borderRadius:'20px',padding:'32px'}}><JustificationImpactChart/></div>
+          <div style={{flexBasis:'200', flexGrow:'1' , display: 'flex', justifyContent: 'center', alignItems: 'center'}} ><AbsenceTable/></div>
           
           
         </div>
