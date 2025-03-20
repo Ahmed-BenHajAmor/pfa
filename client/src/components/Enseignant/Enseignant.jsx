0933ec8249d645fbc7236419f948ed4bc004d0bc
Ahmed <ahmed.benhajamorr@gmail.com>
2025-03-20 14:05:24 +0100

    admin db

--- client/src/components/Enseignant/Enseignant.jsx
+++ client/src/components/Enseignant/Enseignant.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Enseignant.css'
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
 import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 import { Sidebar } from '..';
 import { Outlet } from 'react-router';
+import { Context } from '../../App';
 
 
 
 
 
 function Enseignant() {
-  
+  const {user} = useContext(Context)
   const [sideBarLinksArray, setSideBarLinksArray] = useState([
     {text: "Suivie présence", Icon: HowToRegIcon, highlighted: true, route:'/'}, {text: "Justifications vérifiées", Icon: CheckCircleRoundedIcon, highlighted: false,
      route:'/envoyer-justification'}
     ])
   return (
     <>
-      <Sidebar userinfo={{username: "MR.Faouzi moussa"}} links={sideBarLinksArray} setSideBarLinksArray={setSideBarLinksArray}/>
+      <Sidebar userinfo={{username: `${user?.prenom} ${user?.nom}`}} links={sideBarLinksArray} setSideBarLinksArray={setSideBarLinksArray}/>
       <Outlet />
     </>
   )
