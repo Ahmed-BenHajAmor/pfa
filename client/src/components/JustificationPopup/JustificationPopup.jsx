16d74f3a74cb6d8e956f6c80bfb26d8e800f573f
Ahmed <ahmed.benhajamorr@gmail.com>
2025-03-20 09:33:04 +0100

    admin db

--- client/src/components/JustificationPopup/JustificationPopup.jsx
+++ client/src/components/JustificationPopup/JustificationPopup.jsx
@@ -3,6 +3,26 @@ import './JustificationPopup.css'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 function JustificationPopup({popupInfo, setPopupInfo}) {
+    console.log(popupInfo );
+    
+    const dataToShow = {
+        cin: popupInfo.data.cin,
+        nom: popupInfo.data.nom,
+        prenom: popupInfo.data.prenom,
+        motif: popupInfo.data.motif,
+        date_soumission: popupInfo.data.date_soumission,
+        date_et_heure_de_debut: popupInfo.data.date_et_heure_de_debut,
+        date_et_heure_de_fin: popupInfo.data.date_et_heure_de_fin,
+    }
+    const keysObject = {
+        cin: "cin",
+        nom: "nom",
+        motif: "motif",
+        date_soumission: "date soumission",
+        date_et_heure_de_debut: "date et heure de debut",
+        date_et_heure_de_fin: "date et heure de fin",
+        prenom: "prenom",
+    }
 
   return (
     <section className="justification-popup-container">
@@ -19,14 +39,34 @@ function JustificationPopup({popupInfo, setPopupInfo}) {
         <table>
             <tbody>
 
-            {Object.entries(popupInfo.data).map(([key, value])=>{
+            {Object.entries(dataToShow).map(([key, value])=>{
                 return (
                     <tr>
-                        <td>{key}</td>
+                        <td>{keysObject[key]}</td>
                         <td>{value}</td>
                     </tr>
                 )
             })}
+            <tr>
+                <td>statut</td>
+                <td>{popupInfo.data.id_etudiant == null ? 'Enseignant' : "Etudiant"}</td>
+            </tr>
+            {popupInfo.data.id_etudiant !== null &&
+            <>
+            <tr>
+                <td>section</td>
+                <td>{popupInfo.data.nom_section}</td>
+            </tr>
+            <tr>
+                <td>groupe</td>
+                <td>{popupInfo.data.groupe}</td>
+            </tr>
+            </>
+            }
+            <tr>
+                <td>piece jointe</td>
+                <td><a href="#">Telecharger</a></td>
+            </tr>
             </tbody>
         </table>
         <div className='decision-btns'>
