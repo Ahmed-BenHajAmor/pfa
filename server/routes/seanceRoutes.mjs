5702efe5a48e0bd1fafa301967fac69f6162ebfb
Ahmed <ahmed.benhajamorr@gmail.com>
2025-03-23 09:41:28 +0100

    changes

--- server/routes/seanceRoutes.mjs
+++ server/routes/seanceRoutes.mjs
@@ -1,5 +1,7 @@
 import express from 'express';
 import pool from '../db_connection/db.mjs';
+import verifyToken from '../utils/midelwares/verifyToken.mjs';
+import v4 from 'uuid'
 
 const seanceRoutes = express.Router();
 
@@ -19,6 +21,19 @@ seanceRoutes.get('/session', async (req, res) => {
     res.status(500).json({ error: 'Erreur serveur' });
   }
 });
+
+
+seanceRoutes.patch("/session/state", verifyToken, async (req, res)=>{
+  const {id_session, id_seance, id_etudiant} = req.body
+  if(!(id_session && id_seance && id_etudiant)){
+    res.sendStatus(500)
+  }
+  try{
+    await pool.query('');
+  }catch(err){
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+})
 
 
 export default seanceRoutes;
